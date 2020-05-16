e8f8baac78c24a17569ce0797cd000345ccc5fcd
naufalandika_ <naufalandika@comeapp.id>
2020-05-16 16:39:04 +0700

    Add rajaongkir utils for getting province list

---
+++ utils/rajaongkir.js
@@ -0,0 +1,21 @@
+const axios = require('axios')
+
+const headers = {
+    'key': process.env.RAJAONGKIR_APIKEY
+}
+
+const getProvinces = async () => {
+    try {
+        const response = await axios.get(process.env.RAJAONGKIR_URL + '/province', {
+            headers
+        })
+
+        return response.data.rajaongkir.results
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+module.exports = {
+    getProvinces
+}
